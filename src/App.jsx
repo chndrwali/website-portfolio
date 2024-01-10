ec3529ec7c701136c33f8552052da9e259d6edb3
chndrwali <chndrwali@gmail.com>
2024-01-10 14:34:08 +0700

    add styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,70 @@
-import { useState } from 'react'
-function App() {
-  const [count, setCount] = useState(0)
+import React from "react";
+import { motion } from "framer-motion";
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img  className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="text-7xl">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+import styles from "./style";
+import {
+  Navbar,
+  Hero,
+  Education,
+  SkillsAndExperience,
+  ExtraCurricular,
+  Footer,
+  OpenSource,
+  Projects,
+  BlogPosts,
+  Loading
+} from "./components";
 
-export default App
+const App = () => {
+  const [isLoading,setIsLoading] = React.useState(true);
+
+  React.useEffect(()=>{
+    setTimeout(()=>{setIsLoading(false)},1600);
+  },[])
+
+  if(isLoading){
+    return (
+      <div className="bg-primary w-full overflow-hidden">
+        <Loading/>
+      </div>
+    )
+  }else{
+    return (
+      // A div to wrap the entire application
+    <div className="bg-primary w-full overflow-hidden">
+      <motion.section
+        initial={{ x: -100, opacity: 0.25 }}
+        animate={{ x: 0, opacity: 1 }}
+        transition={{ duration: 1 }}
+      >
+        <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+          <div className={`${styles.boxWidth}`}>
+            <Navbar />
+          </div>
+        </div>
+
+        <div className={`bg-primary ${styles.flexStart}`}>
+          <div className={`${styles.boxWidth}`}>
+            <Hero />
+          </div>
+        </div>
+
+        <div className={`bg-primary ${styles.flexCenter} ${styles.paddingX} `}>
+          <div className={`${styles.boxWidth}`}>
+            <SkillsAndExperience />
+            <Education />
+            <Projects />
+            <BlogPosts enabled={false} />
+            <OpenSource />
+            <ExtraCurricular />
+          </div>
+        </div>
+        <Footer />
+      </motion.section>
+    </div>
+
+    );
+  }
+};
+
+export default App;
