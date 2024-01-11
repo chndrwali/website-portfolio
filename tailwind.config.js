6dbc1376b728ecb15cf32a45d7be857863735d38
chndrwali <chndrwali@gmail.com>
2024-01-11 15:41:34 +0700

    fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,7 @@ module.exports = {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
-      colors: {s
+      colors: {
         blueColors: '#255BE0',
         darkColors: '#191E23',
         lightColors: '#F2F2F2',
