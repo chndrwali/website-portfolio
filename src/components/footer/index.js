4838462fa5a165c3f1e75ae3895ef5a66220b1c1
chndrwali <chndrwali@gmail.com>
2024-01-11 16:29:46 +0700

    add footer

---
+++ src/components/footer/index.js
@@ -0,0 +1,9 @@
+export default function Footer() {
+    return (
+      <div className="px-setting  ">
+        <div className=" border-t border-zinc-400 2xl:w-3/4 m-auto">
+          <h3>© 2024 Candra Wali Sanjaya | All Rights Reserved</h3>
+        </div>
+      </div>
+    );
+  }
