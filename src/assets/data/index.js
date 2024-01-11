8e3d7f93f4c632c4d03377546303a4b0a2fe116c
chndrwali <chndrwali@gmail.com>
2024-01-11 16:16:54 +0700

    upload assets

---
+++ src/assets/data/index.js
@@ -0,0 +1,140 @@
+import {
+  Nfttheme,
+  RumahImpian,
+  Selena,
+  Chat,
+  NftDesign,
+  RumahImpianDesign,
+  SuroriversideDesign,
+  Mkstp,
+  Wumbo,
+  Wedding,
+  InstagramPost,
+  Github,
+  Git,
+  Npm,
+  Yarn,
+  Vscode,
+  Figma,
+  Canva,
+  Html,
+  Css,
+  React,
+  Nodejs,
+  Bootstrap,
+  Tailwindcss,
+  JavaScript,
+  FramerMotion,
+  Store,
+} from '../images';
+export const SlicingData = [
+  {
+    title: 'Nft-theme',
+    desc: 'Discovery rare digital art and Collect Sell Extraordinary NFTs',
+    image: Nfttheme,
+    build: 'Next js, TailwindCss, Typescript ',
+    link: 'https://nfttheme-nextjs.vercel.app/',
+    github: 'https://github.com/Tri-Hartono/Nfttheme-nextjs',
+    color: '#14141F',
+  },
+  {
+    title: 'Store Fake Api',
+    desc: 'NEW SEASON CHECK OUT ALL THE TRENDS',
+    image: Store,
+    build: 'Next js, TailwindCss, Typescript ',
+    link: 'https://tristore.vercel.app/',
+    github: 'https://github.com/Tri-Hartono/store',
+    color: '#000',
+  },
+  {
+    title: 'Rumah Impian',
+    desc: 'Rumah Impian hadir untuk temukan rumah terbaik untuk dijual maupun disewa dengan sumber terpercaya',
+    image: RumahImpian,
+    build: 'React Js, TailwindCss ',
+    link: 'https://rumah-impian-roan.vercel.app/',
+    github: 'https://github.com/Tri-Hartono/Rumah-impian',
+    color: '#01804D',
+  },
+  {
+    title: 'Chat N ReChat',
+    desc: 'Sebuah aplikasi chatting yang bisa bikin kamu lupa diri, tapi tidak lupa jasa pahlawan!',
+    image: Chat,
+    build: 'Next js, TailwindCss ',
+    link: 'https://chat-nrechat.vercel.app/',
+    github: 'https://github.com/Tri-Hartono/ChatNrechat',
+    color: '#4A72FF',
+  },
+  {
+    title: 'Wumbo',
+    desc: 'Figma social media content templates for branding, marketing, insights, and more. Free for personal and commercial use!',
+    image: Wumbo,
+    build: 'Html, TailwindCss, Javascript ',
+    link: 'https://wumbo-one.vercel.app/',
+    github: 'https://github.com/Tri-Hartono/ArrayId-Wumbo',
+    color: '#0C4A6E',
+  },
+  {
+    title: 'Array Id Mkstp',
+    desc: 'Social Media Tool Yang Tidak Akan Memberikan Kamu Insight Apapun',
+    image: Mkstp,
+    build: 'Html, TailwindCss , Javascript ',
+    link: 'https://arrayid-mstskp-two.vercel.app/',
+    github: 'https://github.com/Tri-Hartono/Arrayid-mstskp',
+    color: '#1B1525',
+  },
+  {
+    title: 'Selena Portofolio',
+    desc: 'Sebuah aplikasi Portofolio Selena',
+    image: Selena,
+    build: 'Html, Css, Javascript ',
+    link: 'https://selena-blue.vercel.app/',
+    github: 'https://github.com/Tri-Hartono/selena',
+    color: '#2C2D2A',
+  },
+];
+
+export const DesignData = [
+  {
+    title: 'Web Design',
+    image: NftDesign,
+  },
+  {
+    title: 'Web Design',
+    image: RumahImpianDesign,
+  },
+  {
+    title: 'Web Design',
+    image: SuroriversideDesign,
+  },
+  {
+    title: 'Web Design',
+    image: Wedding,
+  },
+  {
+    title: 'Instagram ',
+    image: InstagramPost,
+  },
+];
+
+//data skills
+
+export const ToolsData = {
+  Git: { name: 'Git', src: Git, desc: 'Version control' },
+  Github: { name: 'Github', src: Github, desc: 'Git hosting' },
+  NPM: { name: 'NPM', src: Npm, desc: 'Package manager' },
+  Yarn: { name: 'Yarn', src: Yarn, desc: 'Dependency tool' },
+  VSCode: { name: 'VSCode', src: Vscode, desc: 'Code editor' },
+  Figma: { name: 'Figma', src: Figma, desc: 'Design' },
+  Canva: { name: 'Canva', src: Canva, desc: 'design' },
+};
+
+export const StacksData = {
+  HTML: { name: 'HTML', src: Html, desc: 'Language' },
+  CSS: { name: 'CSS', src: Css, desc: 'Language' },
+  JS: { name: 'Javascript', src: JavaScript, desc: 'Language' },
+  React: { name: 'ReactJS', src: React, desc: 'Libary' },
+  NodeJS: { name: 'NodeJS', src: Nodejs, desc: 'Runtime' },
+  Tailwind: { name: 'Tailwind', src: Tailwindcss, desc: 'Framework' },
+  Bootstrap: { name: 'Bootstrap', src: Bootstrap, desc: 'Framework' },
+  FramerMotion: { name: 'FramerMotion', src: FramerMotion, desc: 'Animation' },
+};
---
+++ src/assets/images/index.js
@@ -0,0 +1,65 @@
+import Nfttheme from './imgproject/project.jpg';
+import RumahImpian from './imgproject/Rumahimpian.jpg';
+import Selena from './imgproject/Selena.jpg';
+import Chat from './imgproject/Chat.jpg';
+import Mkstp from './imgproject/mkstp.jpg';
+import Wumbo from './imgproject/wumbo.jpg';
+import Logo from './imgproject/Logo.svg';
+import Profile from './imgproject/Profile.png';
+import Thanks from './imgproject/Thanks.png';
+import HandIcon from './imgproject/HandIcon.png';
+import NftDesign from './imgdesign/nftdesign.jpg';
+import RumahImpianDesign from './imgdesign/rumahimpian.jpg';
+import SuroriversideDesign from './imgdesign/Suroriverside.jpg';
+import InstagramPost from './imgdesign/InstagramPost.jpg';
+import Wedding from './imgdesign/Wedding.jpg';
+import Html from './imgskill/html.png';
+import Css from './imgskill/Css3.png';
+import JavaScript from './imgskill/javascript.png';
+import Tailwindcss from './imgskill/Tailwind.png';
+import Bootstrap from './imgskill/bootstrap.png';
+import Canva from './imgskill/canva.png';
+import Figma from './imgskill/figma.png';
+import FramerMotion from './imgskill/framermotion.png';
+import Git from './imgskill/git.png';
+import Github from './imgskill/github.png';
+import Nodejs from './imgskill/nodejs.png';
+import Npm from './imgskill/npm.png';
+import React from './imgskill/react.png';
+import Vscode from './imgskill/Vscode.png';
+import Yarn from './imgskill/yarn.png';
+import Store from './imgproject/store.jpg';
+
+export {
+  HandIcon,
+  Thanks,
+  Profile,
+  Logo,
+  Nfttheme,
+  RumahImpian,
+  Selena,
+  Chat,
+  NftDesign,
+  RumahImpianDesign,
+  SuroriversideDesign,
+  Mkstp,
+  Wumbo,
+  InstagramPost,
+  Wedding,
+  Html,
+  Css,
+  JavaScript,
+  Tailwindcss,
+  Bootstrap,
+  Canva,
+  Figma,
+  FramerMotion,
+  Git,
+  Github,
+  Nodejs,
+  Npm,
+  React,
+  Vscode,
+  Yarn,
+  Store,
+};
