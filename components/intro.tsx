2b2b7fd2b740249239c31c57f24a8bd067020826
chndrwali <chndrwali@gmail.com>
2024-01-12 20:10:00 +0700

    fix intro responsive mobile, change profil picture, add react-type-animation

--- components/intro.tsx
+++ components/intro.tsx
@@ -7,11 +7,12 @@ import { motion } from 'framer-motion';
 import Link from 'next/link';
 import { BsArrowRight, BsLinkedin } from 'react-icons/bs';
 import { HiDownload } from 'react-icons/hi';
-import { FaGithubSquare, FaWhatsappSquare } from 'react-icons/fa';
+import { FaGithub, FaWhatsapp, FaInstagram } from 'react-icons/fa';
 import { useSectionInView } from '@/lib/hooks';
 import { useActiveSectionContext } from '@/context/active-section-context';
 import { useLanguageContext } from '@/context/language-context';
 import { TYPHOGRAPHY } from '@/lib/data';
+import { TypeAnimation } from 'react-type-animation';
 
 export default function Intro() {
   const { ref } = useSectionInView('Home', 0.5);
@@ -19,8 +20,8 @@ export default function Intro() {
   const { language } = useLanguageContext();
 
   return (
-    <section ref={ref} id="home" className="mb-28 max-w-[50rem] text-center sm:mb-0 scroll-mt-[100rem]">
-      <div className="flex items-center justify-center mb-8">
+    <section ref={ref} id="home" className="mb-8 md:mb-0 max-w-[50rem] mx-auto text-center md:scroll-mt-[100rem]">
+      <div className="flex items-center justify-center mb-4 md:mb-8">
         <motion.div
           initial={{ opacity: 0, scale: 0 }}
           animate={{ opacity: 1, scale: 1 }}
@@ -29,26 +30,30 @@ export default function Intro() {
             duration: 0.2,
           }}
         >
-          <Image src="/hello.webp" alt="Ricardo portrait" width="192" height="192" quality="95" priority={true} className="h-24 w-24 rounded-full object-cover border-[0.35rem] border-white shadow-xl" />
+          <Image src="/hello.webp" alt="Candra portrait" width="200" height="200" priority={true} className="h-20 w-20 md:h-40 md:w-40 rounded-full object-cover border-[0.35rem] border-white shadow-xl" />
         </motion.div>
       </div>
 
-      <motion.h1 className="mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl" initial={{ opacity: 0, y: 100 }} animate={{ opacity: 1, y: 0 }}>
+      <motion.h1 className="mb-4 mt-2 md:mb-10 md:mt-4 px-4 text-base md:text-4xl font-medium !leading-[1.5]">
         {language === 'EN' ? (
           <>
-            <span className="font-bold">Hello, I'm Candra Wali Sanjaya.</span> I'm a <span className="font-bold">Frontend developer</span> with <span className="font-bold">2 years+</span> of experience. I enjoy building{' '}
-            <span className="italic">sites & apps</span>. My focus is <span className="underline">React (Next.js) & React Native</span>.
+            <TypeAnimation sequence={["I'am Candra Wali Sanjaya ", 2000, "I'am a Front End Developer", 2000, "I'am a Machine Learning Developer", 2000]} repeat={Infinity} />
+            <p>
+              Hi everyone <span>👋🏼</span>, welcome to my portfolio website
+            </p>
           </>
         ) : (
           <>
-            <span className="font-bold">Halo, saya Candra Wali Sanjaya.</span> Saya seorang <span className="font-bold">Frontend Developer</span> dengan <span className="font-bold">pengalaman 2 tahun lebih</span>. Saya senang dalam membuat{' '}
-            <span className="italic">website & aplikasi</span>. Fokus saya adalah <span className="underline">React (Next.js) & React Native</span>.
+            <TypeAnimation sequence={["I'am Candra Wali Sanjaya ", 2000, "I'am a Front End Developer", 2000, "I'am a Machine Learning Developer", 2000]} repeat={Infinity} />
+            <p>
+              Halo semuanya <span>👋🏼</span>, selamat datang di situs portofolio saya
+            </p>
           </>
         )}
       </motion.h1>
 
       <motion.div
-        className="flex flex-col sm:flex-row items-center justify-center gap-2 px-4 text-lg font-medium"
+        className="flex flex-col md:flex-row items-center justify-center gap-2 px-4 text-sm md:text-lg font-medium"
         initial={{ opacity: 0, y: 100 }}
         animate={{ opacity: 1, y: 0 }}
         transition={{
@@ -57,7 +62,7 @@ export default function Intro() {
       >
         <Link
           href="#contact"
-          className="group bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition"
+          className="group bg-gray-900 text-white px-4 md:px-7 py-2 md:py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition"
           onClick={() => {
             setActiveSection('Contact');
             setTimeOfLastClick(Date.now());
@@ -67,7 +72,7 @@ export default function Intro() {
         </Link>
 
         <a
-          className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10"
+          className="group bg-white px-4 md:px-7 py-2 md:py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10"
           href="/my_cv.pdf"
           download
           aria-label="download cv candra wali sanjaya"
@@ -77,7 +82,7 @@ export default function Intro() {
 
         <div className="flex items-center gap-2">
           <a
-            className="bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
+            className="bg-white p-2 md:p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
             href="https://www.linkedin.com/in/chndrwali/"
             target="_blank"
             aria-label="Linkedin Candra Wali Sanjaya"
@@ -86,21 +91,29 @@ export default function Intro() {
           </a>
 
           <a
-            className="bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
+            className="bg-white p-2 md:p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] md:text-lg rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
             href="https://github.com/chndrwali"
             target="_blank"
             aria-label="Gihtub Candra Wali Sanjaya"
           >
-            <FaGithubSquare />
+            <FaGithub />
           </a>
 
           <a
-            className="bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
+            className="bg-white p-2 md:p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] md:text-lg rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
             href="https://wa.me/6289652648201?text=Hello Candra"
             target="_blank"
             aria-label="Whatsapp Candra Wali Sanjaya"
           >
-            <FaWhatsappSquare />
+            <FaWhatsapp />
+          </a>
+          <a
+            className="bg-white p-2 md:p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] md:text-lg rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
+            href="https://instagram.com/chndrwali"
+            target="_blank"
+            aria-label="Instagram Candra Wali Sanjaya"
+          >
+            <FaInstagram />
           </a>
         </div>
       </motion.div>
